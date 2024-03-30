287dc3365b9060ea99c1cdba1b8e0cd2b54e727b
danieldf01 <ddx1015@alu.ubu.es>
2024-03-30 19:28:33 +0100

    cleaned up code

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def home():  # put application's code here
-#     return 'Hello <h1>World!<h1>'
-#
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello <h1>{name}<h1>"
-#
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("home"))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
