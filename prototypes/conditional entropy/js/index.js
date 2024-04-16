97b7bb14c4334205a840d9400fea801f17688dd8
danieldf01 <d.drefs.fernandes@gmail.com>
2024-04-16 13:38:43 +0200

    Add Category and Remove Category buttons added #25

--- prototypes/conditional entropy/js/index.js
+++ prototypes/conditional entropy/js/index.js
@@ -51,9 +51,14 @@ function calcEntropyCat(rowSums, tBodyRef, instanceVals){
 
 }
 
-function calcCondEntropy(){
+function calcCondEntropy(tableNr){
     // Calculate ratios and Entropies for each category first
-    var table = document.getElementById('table-cond-entropy-1');
+    if (tableNr == 1){
+        var table = document.getElementById('table-cond-entropy-1');
+    } else{
+        var table = document.getElementById('table-cond-entropy-2');
+    }
+    
     var tBodyRef = table.getElementsByTagName('tbody')[0];
     var instanceVals = table.getElementsByTagName('input');
     var rowSumsRatios = calcRatio(tBodyRef, instanceVals);
@@ -65,5 +70,89 @@ function calcCondEntropy(){
     for (var i = 0; i < entropies.length; i++) {
         condEntropy += ratioVals[i] * entropies[i];
     }
-    document.getElementById('ce').innerHTML = condEntropy;
+    if(tableNr == 1){
+        document.getElementById('ce1').innerHTML = condEntropy;
+    } else{
+        document.getElementById('ce2').innerHTML = condEntropy;
+    }
+}
+    
+function removeCategory(){
+    var table = document.getElementById('table-cond-entropy-2');
+    var tBodyRef = table.getElementsByTagName('tbody')[0];
+    table.deleteRow(tBodyRef.rows.length);
+
+    // add "Category remove button" to the now last row
+    if(tBodyRef.rows.length >= 4) {
+        var removeButton = document.createElement("div");
+        removeButton.classList.add("btn");
+        removeButton.classList.add("btn-outline-danger");
+        removeButton.setAttribute("onclick", "removeCategory()");
+        removeButton.innerHTML = "-";
+
+        cell = tBodyRef.rows[tBodyRef.rows.length - 1].cells[0]
+        cell.appendChild(removeButton);
+    }
+}
+
+function addCategory(){
+    var table = document.getElementById('table-cond-entropy-2');
+    var tBodyRef = table.getElementsByTagName('tbody')[0];
+
+    var catCount = +tBodyRef.rows[tBodyRef.rows.length - 1].cells[0].id[1] + 1;
+    var newRow = tBodyRef.insertRow();
+    var catCell = newRow.insertCell();
+    var class1Cell = newRow.insertCell();
+    var class2Cell = newRow.insertCell();
+    var ratioCell = newRow.insertCell();
+    var entropyCell = newRow.insertCell();
+
+    // Category cell
+    catCell.id = "c" + catCount;
+    catLabel = document.createElement("label");
+    catLabel.classList.add("form-control-plaintext");
+    catLabel.innerHTML = "Category " + +catCount + ":";
+    catCell.appendChild(catLabel);
+
+    removeButton = document.createElement("div");
+    removeButton.classList.add("btn");
+    removeButton.classList.add("btn-outline-danger");
+    removeButton.setAttribute("onclick", "removeCategory()");
+    removeButton.innerHTML = "-";
+    catCell.appendChild(removeButton);
+
+    // Class 1 cell
+    class1Input = document.createElement("input");
+    class1Input.setAttribute("value", "0");
+    class1Input.setAttribute("type", "text");
+    class1Input.classList.add("form-control");
+    class1Cell.appendChild(class1Input);
+
+    // Class 2 cell
+    class2Input = document.createElement("input");
+    class2Input.setAttribute("value", "0");
+    class2Input.setAttribute("type", "text");
+    class2Input.classList.add("form-control");
+    class2Cell.appendChild(class2Input);
+
+    // Ratio cell
+    ratioLabel = document.createElement("label");
+    ratioLabel.classList.add("form-control-plaintext");
+    ratioLabel.innerHTML = "0";
+    ratioCell.appendChild(ratioLabel);
+
+    // Entropy cell
+    entropyLabel = document.createElement("label");
+    entropyLabel.classList.add("form-control-plaintext");
+    entropyLabel.innerHTML = "0";
+    entropyCell.appendChild(entropyLabel);
+
+    document.getElementById('ce2cell').setAttribute("rowspan", catCount.toString());
+
+    // remove "Category remove button" of previous input group so that there is only one
+    if(tBodyRef.rows.length >= 5) {
+        cell = tBodyRef.rows[tBodyRef.rows.length - 2].cells[0]
+        button = cell.getElementsByTagName("div")[0];
+        cell.removeChild(button);
+    }
 }
