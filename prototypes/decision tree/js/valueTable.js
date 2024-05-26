8948845bfb98847fd76a8b9bfa4ec885dc07aadf
danieldf01 <d.drefs.fernandes@gmail.com>
2024-05-26 14:05:46 +0200

    Creation of value table for leaf nodes added #63

--- prototypes/decision tree/js/valueTable.js
+++ prototypes/decision tree/js/valueTable.js
@@ -11,28 +11,56 @@ const bodyRowValCount = 4;
 const indexBodyCondEntropy = 4;
 const indexBodyInfoGain = 5;
 
-function createBody(step, tableEl){
+const valueLabelsLength = 2;
+
+function createHeaderForNode(tableEl) {
+    var header = document.createElement('thead');
+    var headerRow1 = document.createElement('tr');
+    for (var i = 0; i < firstHeaderRowCols; i++) {
+        var headerCell = document.createElement('th');
+        if (i === indexHeaderLabelCol) {
+            headerCell.colSpan = '2';
+            headerCell.textContent = label;
+            headerRow1.appendChild(headerCell);
+        } else {
+            headerCell.style.borderBottom = 'none';
+            headerRow1.appendChild(headerCell);
+        }
+    }
+    header.appendChild(headerRow1);
+
+    var headerRow2 = document.createElement('tr');
+    headerRow2.classList.add('sticky-top');
+    for (var i = 0; i < secondHeaderRowCols; i++) {
+        var headerCell = document.createElement('th');
+        headerCell.textContent = secondHeaderRowColLabels[i];
+        headerRow2.appendChild(headerCell);
+    }
+    header.appendChild(headerRow2);
+
+    tableEl.appendChild(header);
+}
+
+function createBodyForNode(tableEl, step) {
     var body = document.createElement('tbody');
     body.classList.add('table-group-divider');
-    var tableContents = valueTableGroups[step];
+    var tableContents = valueTableGroups[step - 1];
     var maxInfoGain = 0;
     var maxInfoGainCell = null;
-    console.log(tableContents);
 
     // Loop through each feature
-    for (var i = 0; i < tableContents.length; i++){
+    for (var i = 0; i < tableContents.length; i++) {
         var feature = tableContents[i];
         var featureName = Object.keys(feature)[0];
         var featureValues = Object.values(feature)[0];
-        console.log(featureValues);
 
         // Loop through each feature's values/rows
-        for (var j = 0; j < featureValues.length; j++){
+        for (var j = 0; j < featureValues.length; j++) {
             var currentRowVals = featureValues[j];
             var bodyRow = document.createElement('tr');
 
             // First row of the feature carries the feature name
-            if (j === 0){
+            if (j === 0) {
                 var bodyCell = document.createElement('td');
                 bodyCell.rowSpan = featureValues.length;
                 bodyCell.style.verticalAlign = 'middle';
@@ -41,29 +69,29 @@ function createBody(step, tableEl){
             }
 
             // Create column for each value
-            for (var k = 0; k < bodyRowValCount; k++){
+            for (var k = 0; k < bodyRowValCount; k++) {
                 var bodyCell = document.createElement('td');
                 // Label values
-                if (k === 1){
+                if (k === 1) {
                     bodyCell.textContent = currentRowVals[k][0];
                     bodyRow.appendChild(bodyCell);
                     bodyCell = document.createElement('td');
                     bodyCell.textContent = currentRowVals[k][1];
-                } else{
+                } else {
                     bodyCell.textContent = currentRowVals[k];
                 }
                 bodyRow.appendChild(bodyCell);
             }
 
             // First row of the feature carries the conditional entropy and info gain
-            if (j === 0){
-                for (var k = indexBodyCondEntropy; k <= indexBodyInfoGain; k++){
+            if (j === 0) {
+                for (var k = indexBodyCondEntropy; k <= indexBodyInfoGain; k++) {
                     var bodyCell = document.createElement('td');
                     bodyCell.rowSpan = featureValues.length;
                     bodyCell.style.verticalAlign = 'middle';
                     bodyCell.textContent = currentRowVals[k];
                     bodyRow.appendChild(bodyCell);
-                    
+
                     // Check if current info gain is the max info gain
                     if (k === indexBodyInfoGain && currentRowVals[k] > maxInfoGain) {
                         maxInfoGain = currentRowVals[k];
@@ -71,7 +99,7 @@ function createBody(step, tableEl){
                     }
                 }
             }
-            
+
             body.appendChild(bodyRow);
         }
     }
@@ -82,45 +110,85 @@ function createBody(step, tableEl){
     tableEl.appendChild(body);
 }
 
-function createValueTable(){
+function createValueTableForNode(tableEl, step) {
+    // Header
+    createHeaderForNode(tableEl);
+
+    // Body
+    createBodyForNode(tableEl, step);
+}
+
+function createValueTableForLeaf(tableEl, step) {
+    // Header
+    var header = document.createElement('thead');
+    var headerRow1 = document.createElement('tr');
+    var headerCell = document.createElement('th');
+    headerCell.colSpan = '2';
+    headerCell.textContent = label;
+    headerRow1.appendChild(headerCell);
+    header.appendChild(headerRow1);
+
+    var headerRow2 = document.createElement('tr');
+    headerCell = document.createElement('th');
+    headerCell.textContent = labelValues[0];
+    headerRow2.appendChild(headerCell);
+    headerCell = document.createElement('th');
+    headerCell.textContent = labelValues[1];
+    headerRow2.appendChild(headerCell);
+    header.appendChild(headerRow2);
+
+    tableEl.appendChild(header);
+
+    // Body
+    var body = document.createElement('tbody');
+    body.classList.add('table-group-divider');
+
+    var bodyRow = document.createElement('tr');
+    var bodyCell = document.createElement('td');
+    bodyCell.textContent = valueTableGroups[step - 1][0];
+    bodyRow.appendChild(bodyCell);
+    bodyCell = document.createElement('td');
+    bodyCell.textContent = valueTableGroups[step - 1][1];
+    bodyRow.appendChild(bodyCell);
+    body.appendChild(bodyRow);
+
+    tableEl.appendChild(body);
+
+    // Foot
+    var foot = document.createElement('tfoot');
+    foot.classList.add('table-group-divider');
+    var footRow = document.createElement('tr');
+    var footCell = document.createElement('td');
+    footCell.colSpan = '2';
+    footCell.textContent = 'All labels have the same value';
+    footRow.appendChild(footCell);
+    foot.appendChild(footRow);
+
+    tableEl.appendChild(foot);
+}
+
+function createValueTable(step) {
     var tableDiv = document.getElementById('valueTable');
+
+    // Remove table of a previous step
+    if (tableDiv.getElementsByTagName('table')[0] != null) {
+        tableDiv.removeChild(tableDiv.getElementsByTagName('table')[0]);
+    }
 
     // Table element
     var tableEl = document.createElement('table');
     tableEl.classList.add('table');
     tableEl.setAttribute("id", "valueTableEl");
 
-    // Header
-    var header = document.createElement('thead');
-    header.classList.add('sticky-top');
-    var headerRow1 = document.createElement('tr');
-    for (var i = 0; i < firstHeaderRowCols; i++) {
-        var headerCell = document.createElement('th');
-        if(i === indexHeaderLabelCol) {
-            headerCell.colSpan = '2';
-            headerCell.textContent = label;
-            headerRow1.appendChild(headerCell);
-        } else{
-            headerCell.style.borderBottom = 'none';
-            headerRow1.appendChild(headerCell);
-        }
+    if (valueTableGroups[step - 1].length === valueLabelsLength) {
+        createValueTableForLeaf(tableEl, step);
+    } else {
+        createValueTableForNode(tableEl, step);
     }
-    header.appendChild(headerRow1);
-
-    var headerRow2 = document.createElement('tr');
-    for (var i = 0; i < secondHeaderRowCols; i++) {
-        var headerCell = document.createElement('th');
-        headerCell.textContent = secondHeaderRowColLabels[i];
-        headerRow2.appendChild(headerCell);
-    }
-    header.appendChild(headerRow2);
-
-    tableEl.appendChild(header);
-
-    // Body
-    createBody(0, tableEl);
 
     tableDiv.appendChild(tableEl);
 }
 
-document.addEventListener('DOMContentLoaded', createValueTable);
+document.addEventListener('DOMContentLoaded', function () {
+    createValueTable(3);
+});
